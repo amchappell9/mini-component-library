c10c34ac72e166229c365cb07bce795bb84f4d40
amchappell9 <amchappell9@gmail.com>
2021-12-15 21:28:08 -0500

    Adding progress bar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,57 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS } from "../../constants";
+import VisuallyHidden from "../VisuallyHidden";
+
+const SIZES = {
+  small: {
+    "--height": "8px",
+    "--padding": "0px",
+  },
+  medium: {
+    "--height": "12px",
+    "--padding": "0px",
+  },
+  large: {
+    "--height": "24px",
+    "--padding": "4px",
+  },
+};
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: var(--height);
+  padding: var(--padding);
+  border-radius: 8px;
+  background-color: ${COLORS.gray50};
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+`;
+
+const InnerProgressBar = styled.div`
+  height: 100%;
+  width: ${(props) => props.value + "%"};
+  border-radius: 4px;
+  background-color: ${COLORS.primary};
+`;
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  const styles = SIZES[size];
+
+  return (
+    <Wrapper
+      role="progressbar"
+      aria-valuenow={value}
+      aria-valuemin="0"
+      aria-valuemax="100"
+      aria-label="Progress Bar"
+      style={styles}
+    >
+      <InnerProgressBar value={value}></InnerProgressBar>
+      <VisuallyHidden>{value}%</VisuallyHidden>
+    </Wrapper>
+  );
 };
 
 export default ProgressBar;
